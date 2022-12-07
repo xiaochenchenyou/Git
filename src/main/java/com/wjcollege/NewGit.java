fe07289d180a02f28d3b7c7e3974978c68b3a896
Chenwei <2052629138@qq.com>
2022-12-07 16:48:03 +0800

    thread commit

--- src/main/java/com/wjcollege/NewGit.java
+++ src/main/java/com/wjcollege/NewGit.java
@@ -6,5 +6,6 @@ package com.wjcollege;
 public class NewGit {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("hello hot-fix");
     }
 }
